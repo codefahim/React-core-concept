aa20b66a2d52cf4e39afb27def288d0a5bbee931
Abdullah Al Fahim <50063518+fahimw@users.noreply.github.com>
2021-02-24 17:12:15 +0600

    React use effect

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        Fahim edit this one < code > src / App.js < /code> and save to reload. <
-        /p> <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React <
-        /a> <
-        /header> <
-        /div>
-    );
+  const [users, setUser] = useState([]);
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      {users.map((user) => (
+        <div>
+          <table>
+            <tr>
+              <th>User Name</th>
+              <th>Company Name</th>
+              <th>Email Address</th>
+              <th>City Name</th>
+            </tr>
+            <tr>
+              <td>{user.name}</td>
+              <td>{user.company.name}</td>
+              <td>{user.email}</td>
+              <td> {user.address.city}</td>
+            </tr>
+          </table>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
